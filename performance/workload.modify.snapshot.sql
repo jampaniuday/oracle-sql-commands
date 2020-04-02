fa327789e083f0d844d3cf2dff02ab92e6c705d2
Richard Koranteng <richard@rkkoranteng.com>
2020-04-02 10:33:45 -0400

    Update workload.modify.snapshot.sql

--- performance/workload.modify.snapshot.sql
+++ performance/workload.modify.snapshot.sql
@@ -1,3 +1,4 @@
 -- modify AWR snapshot generation frequencey (this example changes the snapshot generation to every 10 minutes)
 -- ORA-13511: INTERVAL must be in the range (10, 52560000)
+
 exec dbms_workload_repository.modify_snapshot_settings(interval => 10);
