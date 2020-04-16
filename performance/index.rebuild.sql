3223f66f751b98ee76fefc3814f4dfae6b999b23
Richard Koranteng <richard@rkkoranteng.com>
2020-04-16 11:39:43 -0400

    Create index.rebuild.sql

---
+++ performance/index.rebuild.sql
@@ -0,0 +1,16 @@
+-- script: index_rebuild.sql
+-- author: Richard K @ www.rkkoranteng.com
+-- description: rebuild index belonging to a schema
+-- usage: @index_rebuild.sql <schema>
+
+set feedback off
+set hea off pages 0
+set echo off
+set pagesize 0
+set linesize 500
+set verify off
+set wrap off
+define schema = '&1';
+spool /tmp/rebuild_index.sql
+select 'alter index &schema'||'.'|| index_name || ' rebuild;' from dba_indexes where owner=upper('&schema');
+spool off
